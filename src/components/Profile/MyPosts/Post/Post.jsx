e7cf856ecf284483622d11143c182648a3f313e4
AlexSerdukov12 <73774191+AlexSerdukov12@users.noreply.github.com>
2022-10-13 00:04:23 +0300

    react routes

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -8,6 +8,9 @@ const Post =() =>{
             <div className={styles.item}>
                 <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP_ImaS_qX32yOs7xA8KfC2G0KhqduevErzg&usqp=CAU'/>
                 post 1
+                <div>
+                <span>Like</span>
+                </div>
             </div>
         </div>
 
