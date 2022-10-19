0c40ccd9bb7983bbc212f295e0274a00341a503c
AlexSerdukov12 <73774191+AlexSerdukov12@users.noreply.github.com>
2022-10-19 14:25:23 +0300

    a

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,45 @@
 import React from "react"
+import { NavLink } from "react-router-dom";
 import style from './Dialogs.module.css'
+const DialogItem = (props) =>{
+
+    
+}
 const Dialogs = (props) =>{
     return (
-        <div className={style.contect}>
-            Dialogs
+        <div className={style.global}>
+            <div className={style.friends}>
+
+            
+                  <div className={style.friend}><NavLink to='/messages/1' 
+                  className = { navData => navData.isActive ? style.active : style.friend }>Alex</NavLink>
+                  </div>
+
+                  <div className={style.friend}><NavLink to='/messages/2' 
+                  className = { navData => navData.isActive ? style.active : style.friend }>Karin</NavLink>
+                  </div>
+
+                  <div className={style.friend}><NavLink to='/messages/3' 
+                  className = { navData => navData.isActive ? style.active : style.friend }>Sveta</NavLink>
+                  </div>
+
+            </div>
+            <div className={style.massages}>
+                <div className={style.massage}>
+                    Hello
+                </div>
+                <div className={style.massage}>
+                    Bye
+                </div>
+                <div className={style.massage}>
+                    Hello World
+                </div>
+            </div>
+            
         </div>
     )
 
 }
 export default Dialogs;
+
+
